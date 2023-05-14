87a7ab3b3e81f2e9ec9d34fc78e6ba908a496a7a
Maksym1988 <splien1988@gmail.com>
2023-05-14 17:42:14 +0100

    update 5 task

--- js/task-05.js
+++ js/task-05.js
@@ -2,5 +2,9 @@ const nameInput = document.querySelector('#name-input');
 const nameOutput = document.querySelector('#name-output');
 
 nameInput.addEventListener('input', event => {
-  nameOutput.textContent = event.currentTarget.value;
+  if (nameInput === null) {
+    nameOutput.textContent = 'Anonymous';
+  } else {
+    nameOutput.textContent = event.currentTarget.value;
+  }
 });
