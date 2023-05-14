57476ed77ab5322aae285e74df71d2a1c62ce699
Maksym1988 <splien1988@gmail.com>
2023-05-14 17:18:15 +0100

    finish 10

--- js/task-10.js
+++ js/task-10.js
@@ -3,3 +3,35 @@ function getRandomHexColor() {
     .toString(16)
     .padStart(6, 0)}`;
 }
+
+const input = document.querySelector('input');
+const createBtn = document.querySelector('[data-create]');
+const destroyBtn = document.querySelector('[data-destroy]');
+const newBoxes = document.querySelector('#boxes');
+
+function createBoxes(amount) {
+  destroyBoxes();
+
+  const elements = [];
+
+  for (let i = 0; i < amount; i += 1) {
+    const element = document.createElement('div');
+    element.style.width = `${30 + i * 10}px`;
+    element.style.height = `${30 + i * 10}px`;
+    element.style.backgroundColor = getRandomHexColor();
+    elements.push(element);
+  }
+
+  newBoxes.append(...elements);
+}
+
+function destroyBoxes() {
+  newBoxes.innerHTML = '';
+  input.value = '';
+}
+
+createBtn.addEventListener('click', () => {
+  createBoxes(input.value);
+});
+
+destroyBtn.addEventListener('click', destroyBoxes);
